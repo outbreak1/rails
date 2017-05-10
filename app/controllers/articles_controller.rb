bcbf54e197e213a1ee4aaffe65b2f5c9bcfaa10f
Outbreak <lwiertek@poczta.onet.pl>
2017-05-10 17:48:42 +0200

    Add partials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,12 +15,32 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully update"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-  private
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
+  end
+   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
