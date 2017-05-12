13ab30ee6d36019ebb122cc393133dc5aeb5b715
Outbreak <lwiertek@poczta.onet.pl>
2017-05-12 19:28:41 +0200

    Deploy

--- db/migrate/20170429230259_creat_articles.rb
+++ db/migrate/20170429230259_creat_articles.rb
@@ -2,7 +2,9 @@ class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
       t.string :title
+      t.text :description
 
+      t.timestamps
     end
   end
 end
