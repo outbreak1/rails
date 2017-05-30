edea75aa6ed777d5c904584f43df8c764e5321c8
Outbreak <lwiertek@poczta.onet.pl>
2017-05-31 00:07:22 +0200

    Creat user table and model

--- db/migrate/20170429231047_add_description_to_articles.rb
+++
@@ -1,8 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration[5.1]
-  def change
-    add_column :articles, :description, :text
-    add_column :articles, :created_at, :datetime
-    add_column :articles, :update_at, :datetime
-
-  end
-end
